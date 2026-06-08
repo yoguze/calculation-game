44701b86afc4f232108e6ba4e6949a3532acccad
yoguze <numano6262@keio.jp>
2026-06-08 21:24:10 +0900

    Allow werkzeug for Render python app.py startup

--- app.py
+++ app.py
@@ -772,4 +772,9 @@ def serve_frontend_index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        allow_unsafe_werkzeug=True,
+    )
